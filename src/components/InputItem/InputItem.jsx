c1d0380f5972373c937565b55ec4932661933fd1
Priyankaadev <priyaaswal79@gmail.com>
2024-03-09 19:20:00 +0530

    added react-toastify

--- src/components/InputItem/InputItem.jsx
+++ src/components/InputItem/InputItem.jsx
@@ -1,6 +1,9 @@
 //css imports
 import './InputItem.css'
+import "react-toastify/dist/ReactToastify.css";
 
+//util import
+import { showSuccess } from '../../utils/showToasts';
 
 function InputItem(){
     return(
@@ -9,7 +12,9 @@ function InputItem(){
              type="text"
             placeholder="Add An Item.."
             />
-            <button className='additem-button'>
+            <button className='additem-button'
+            onClick={( )=> showSuccess( "Successfully added item")}
+            >
                 Add
             </button>
         </div>
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -2,19 +2,21 @@
 import Header from "../Header/Header";
 import InputItem from "../InputItem/InputItem";
 import ItemList from "../ItemList/ItemList";
+import { ToastContainer} from 'react-toastify';
 
 //css imports
 import "./ShoppingList.css"
 
 const shoppingItems = [
     {id: 1, name: 'Apples' , quantity: 2},
-    {id: 1, name: 'Rice' , quantity: 1}
+    {id: 2, name: 'Rice' , quantity: 1}
 ]
 
 function ShoppingList(){
     return (
         <>
         <Header />
+        <ToastContainer />
         <div className="current-shopping-list">
             <InputItem/>
             <ItemList
